afacfa33661a45bb3a312d890656b6e783a833aa
kumarsonam <kumarsonam@gmail.com>
2016-06-01 19:11:51 +0530

    Added Basic Card Type

--- app/src/main/java/com/ichi2/apisample/Basic.java
+++ app/src/main/java/com/ichi2/apisample/Basic.java
@@ -1,35 +1,15 @@
 package com.ichi2.apisample;
 
-import android.app.Activity;
 import android.content.ClipData;
-import android.content.ClipDescription;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.ShareCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.text.ClipboardManager;
-import android.util.Log;
-import android.util.SparseBooleanArray;
-import android.view.ActionProvider;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.SubMenu;
+import android.app.Activity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
@@ -38,24 +18,36 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.ichi2.anki.api.AddContentApi;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
+/*
+public class Basic extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
+*/
 
 
-public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+public class Basic extends MainActivity {
     public static final String LOG_TAG = "AnkiDroidApiSample";
     private static final int AD_PERM_REQUEST = 0;
 
     private ListView mListView;
     private ArrayList<HashMap<String, String>> mListData;
 
-    static EditText SharedText1;
+    private String front = "";
+    private String back = "";
 
-    private String personality = "";
-    private String field = "";
-    private String contribution = "";
-    private String criticism = "";
 
+
+
+
+    //EditText SharedText1;
 
     int save = 0;
 
@@ -108,13 +100,13 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         //}
 
         switch (v.getId()) {
-            /*case R.id.button:
+            case R.id.button:
 
                 Button button = (Button) findViewById(R.id.button);
 
                 //Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_LONG).show();
 
-                personality = charseq.toString();
+                front = charseq.toString();
                 //Toast.makeText(MainActivity.this, "\n Personality : " + personality + "\n Field : " + field + "\n Contribution : " + contribution + "\n Criticism: " + criticism, Toast.LENGTH_LONG).show();
 
                 save = 1;
@@ -126,70 +118,53 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
                 Button button2 = (Button) findViewById(R.id.button2);
 
                 //Toast.makeText(MainActivity.this, "Button2 Clicked", Toast.LENGTH_LONG).show();
-                field = charseq.toString();
+                back = charseq.toString();
 
                 save = 1;
 
                 break;
 
-
-            case R.id.button3:
-
-                Button button3 = (Button) findViewById(R.id.button3);
-
-                //Toast.makeText(MainActivity.this, "Button3 Clicked", Toast.LENGTH_LONG).show();
-                contribution = charseq.toString();
-
-                save = 1;
-
-                break;
-
-
-            case R.id.button4:
-
-                Button button4 = (Button) findViewById(R.id.button4);
-
-                //Toast.makeText(MainActivity.this, "Button4 Clicked", Toast.LENGTH_LONG).show();
-                criticism = charseq.toString();
-
-                save = 1;
-
-                break;*/
 
             case R.id.button5:
 
                 Button button5 = (Button) findViewById(R.id.button5);
 
+                //Toast.makeText(MainActivity.this, "Button5 Clicked", Toast.LENGTH_LONG).show();
+                if (save != 1) {
+                    Toast.makeText(Basic.this, "All fields empty!!!", Toast.LENGTH_LONG).show();
+                    break;
+                }
+
                 // Test Code to add basic card
-                final AddContentApi api = new AddContentApi(MainActivity.this);
+                final AddContentApi api = new AddContentApi(Basic.this);
 
                 // Add new deck if one doesn't already exist
-                Long did = api.findDeckIdByName("Cloze");
+                Long did = api.findDeckIdByName("Basic");
 
                 if (did != null) {
                     //Toast.makeText(MainActivity.this, "Found Deck PIN!", Toast.LENGTH_LONG).show();
                 }
 
-                Long mid = api.findModelIdByName("Cloze", 2);
+                Long mid = api.findModelIdByName("Basic", 2);
                 if (mid != null) {
                     //Toast.makeText(MainActivity.this, "Found MID PIN!", Toast.LENGTH_LONG).show();
 
                 }
 
-                String cloze = SharedText1.getText().toString();
 
-                api.addNewNote(mid, did, new String[]{cloze, "", "", ""}, "Cloze");
+                api.addNewNote(mid, did, new String[]{front, back}, "Basic");
 
-                Toast.makeText(MainActivity.this, "\n Add cloze to : " + cloze, Toast.LENGTH_LONG).show();
+                Toast.makeText(Basic.this, "\n Front : " + front + "\n Back : " + back  , Toast.LENGTH_LONG).show();
 
                 break;
+
 
             case R.id.editText:
                 imm.hideSoftInputFromWindow(SharedText1.getWindowToken(), 0); // not working
 
 
             default:
-                Toast.makeText(MainActivity.this, "Please select some text first!!!", Toast.LENGTH_LONG).show();
+                Toast.makeText(Basic.this, "Please select some text first!!!", Toast.LENGTH_LONG).show();
 
         }
 
@@ -200,25 +175,29 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_basic);
 
-        hideKeyboard(MainActivity.this); // not working
+        hideKeyboard(Basic.this); // not working
+
 
         SharedText1 = (EditText) findViewById(R.id.editText);
-        imm = (InputMethodManager) getSystemService(this.INPUT_METHOD_SERVICE); // not working
+/*        imm = (InputMethodManager) getSystemService(this.INPUT_METHOD_SERVICE); // not working
         imm.hideSoftInputFromWindow(SharedText1.getWindowToken(), 0); // not working
 
-
+*/
         // Get intent, action and MIME type
         Intent intent = getIntent();
         String action = intent.getAction();
         String type = intent.getType();
 
+        handleSendText(intent); // Handle text being sent
+        /*
         if (Intent.ACTION_SEND.equals(action) && type != null) {
             if ("text/plain".equals(type)) {
                 handleSendText(intent); // Handle text being sent
             }
-        }
+        }*/
+
 
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
@@ -267,60 +246,8 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         client.disconnect();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        String send_text;
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.issues:
-                //Toast.makeText(MainActivity.this, "issues option selected", Toast.LENGTH_LONG).show();
-                Intent intent_issues = new Intent(MainActivity.this, Issues.class);
-                SharedText1 = (EditText) findViewById(R.id.editText);
-                send_text = SharedText1.getText().toString();
-                intent_issues.putExtra(Intent.EXTRA_TEXT, send_text);
-                startActivity(intent_issues);
-                return true;
-            case R.id.data:
-                //Toast.makeText(MainActivity.this, "data option selected", Toast.LENGTH_LONG).show();
-                Intent intent_data = new Intent(MainActivity.this, Data.class);
-                SharedText1 = (EditText) findViewById(R.id.editText);
-                send_text = SharedText1.getText().toString();
-                intent_data.putExtra(Intent.EXTRA_TEXT, send_text);
-                startActivity(intent_data);
-                return true;
-            case R.id.definition:
-                //Toast.makeText(MainActivity.this, "data option selected", Toast.LENGTH_LONG).show();
-                Intent intent_definition = new Intent(MainActivity.this, Definition.class);
-                SharedText1 = (EditText) findViewById(R.id.editText);
-                send_text = SharedText1.getText().toString();
-                intent_definition.putExtra(Intent.EXTRA_TEXT, send_text);
-                startActivity(intent_definition);
-                return true;
-            case R.id.basic:
-                //Toast.makeText(MainActivity.this, "data option selected", Toast.LENGTH_LONG).show();
-                Intent intent_basic = new Intent(MainActivity.this, Basic.class);
-                SharedText1 = (EditText) findViewById(R.id.editText);
-                send_text = SharedText1.getText().toString();
-                intent_basic.putExtra(Intent.EXTRA_TEXT, send_text);
-                startActivity(intent_basic);
-                return true;
-            case R.id.pin:
-                //Toast.makeText(MainActivity.this, "data option selected", Toast.LENGTH_LONG).show();
-                Intent intent_pin = new Intent(MainActivity.this, Pin.class);
-                SharedText1 = (EditText) findViewById(R.id.editText);
-                send_text = SharedText1.getText().toString();
-                intent_pin.putExtra(Intent.EXTRA_TEXT, send_text);
-                startActivity(intent_pin);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 }
+
+
+
